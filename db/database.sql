27f825d046020a828856bdbaf64a7029fb095e97
luankefei <luankefei@gmail.com>
2021-12-30 00:13:57 +0800

    feat(db): 增加菜谱相关的表

--- db/database.sql
+++ db/database.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS tbl_running_schedule(
   create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   display TINYINT(1) DEFAULT 1,
+  category TINYINT UNSIGNED DEFAULT 1,
   PRIMARY KEY (rid)
 );
 
@@ -46,6 +47,56 @@ CREATE TABLE IF NOT EXISTS tbl_user(
   PRIMARY KEY (uid),
   UNIQUE INDEX idx_email (email)
 ); 
+
+-- 菜谱列表，用于菜谱列表页
+-- CREATE TABLE IF NOT EXISTS tbl_recipe_list(
+--   rlid INT NOT NULL AUTO_INCREMENT,
+--   rid INT NOT NULL,
+--   create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--   PRIMARY KEY (rid),
+-- )
+
+-- 菜谱详情
+CREATE TABLE IF NOT EXISTS tbl_recipe(
+  rid INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL COMMENT '菜品名',
+  foreign_name VARCHAR(255) COMMENT '英文名',
+  intro VARCHAR(255) NOT NULL COMMENT '菜品简介',
+  difficulty TINYINT(1) DEFAULT 0 COMMENT '难度',
+  tags VARCHAR(255) NOT NULL COMMENT '标签id，最多10个，用下划线分割',
+  prepare_takes_time TINYINT(4) NOT NULL DEFAULT 0 COMMENT '准备耗时，单位分钟',
+  cook_takes_time  TINYINT(4) NOT NULL DEFAULT 0 COMMENT '烹饪耗时',
+  additional_takes_time TINYINT(4) NOT NULL DEFAULT 0 COMMENT '额外耗时',
+  total_takes_time TINYINT(4) NOT NULL DEFAULT 0 COMMENT '全部耗时',
+  ingredients VARCHAR(255) NOT NULL COMMENT '食材列表，格式 食材id.数量_食材id2.数量2',
+  directions TEXT NOT NULL COMMENT '制作步骤，json',
+  tips TEXT NOT NULL COMMENT '需要注意的关键点，json',
+  extenions TEXT NOT NULL COMMENT '扩展阅读，json',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (rid),
+  UNIQUE INDEX idx_title (title)
+);
+
+-- 食材表
+CREATE TABLE IF NOT EXISTS tbl_ingredient(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL COMMENT '食材名称',
+  unit VARCHAR(20) NOT NULL COMMENT '食材单位',
+  foreign_name VARCHAR(50) COMMENT '英文名称',
+  calories_per_serving TINYINT(4) COMMENT '每份热量，千卡',
+  PRIMARY KEY(id),
+  UNIQUE INDEX idx_name (name)
+);
+
+-- 标签表
+CREATE TABLE IF NOT EXISTS tbl_tag(
+  tid INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL COMMENT '标签名',
+  PRIMARY KEY(tid),
+  UNIQUE INDEX idx_name (name)
+);
 
 SHOW TABLES;
 
