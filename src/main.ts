010f6436dcd8f801aa6ab9aa43c4978df5791cdf
luankefei <luankefei@gmail.com>
2021-12-30 20:08:36 +0800

    feat(main): add prefix for all routes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO: 抽离到配置文件，区分开发和线上环境
+const PORT = 8000;
+const PREFIX = '/api/me';
+const ENV = 'dev';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // TODO: 抽离到配置文件，区分开发和线上环境
-  const PORT = 8000;
-  const PREFIX = '/api/me';
-  const ENV = 'dev';
+  app.setGlobalPrefix(PREFIX);
 
   await app.listen(PORT, () => {
     const serv = `http://::1:${PORT}${PREFIX}`;
